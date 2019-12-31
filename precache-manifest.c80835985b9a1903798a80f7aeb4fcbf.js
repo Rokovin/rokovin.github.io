970090eca532b2e56a9f0630875c952da2da0af5
Vin <vinjsphp@gmail.com>
2020-01-01 01:25:32 +0800

    Setup React

---
+++ precache-manifest.c80835985b9a1903798a80f7aeb4fcbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2608b68eba1b3b2046080239fa75e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad3f81ce41b37e671fb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e3421efca618c89ce26d",
+    "url": "/static/js/2.76ba3a20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.76ba3a20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ad3f81ce41b37e671fb",
+    "url": "/static/js/main.55415513.chunk.js"
+  },
+  {
+    "revision": "ffc1b33c63074af05d40",
+    "url": "/static/js/runtime-main.249aa804.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
